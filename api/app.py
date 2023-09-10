9bab4748bd9045137edd17df74f683336c6884ca
tipsyx <141881555+tipsyx@users.noreply.github.com>
2023-09-10 17:21:36 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,51 +2,58 @@
 from flask import Flask, jsonify, request, make_response
 import datetime
 import pytz
+import os
 
 app = Flask(__name__)
 app.json_sort_keys = False
 
-slack_name = "Tipsy0"
-github_repo_url = "https://github.com/tipsyx/app1.git"
-github_file_url = "https://github.com/tipsyx/app1/blob/main/api/app.py"
-track = "backend"
+app.secret_key = os.getenv("secret_key")
+slack_name = os.getenv("slack_name")
+github_repo_url = os.getenv("github_repo_url")
+github_file_url = os.getenv("github_file_url")
+track = os.getenv("track")
 
 @app.route('/')
 def index():
     print("Received a request to the index route.")
     return "Hello, World!"
-
-@app.route('/api', methods=['GET'])
-def get_info():
-    slack_name_param = request.args.get('slack_name', slack_name)
-    track_param = request.args.get('track', track)
     
-    if slack_name_param is None:
-        return jsonify({"error": "slack_name parameter is missing"}), 400
-    if track_param is None:
-        return jsonify({"error": "track parameter is missing"}), 400
-        
-    current_day = datetime.datetime.now().strftime('%A')
-    utc_time = datetime.datetime.now(pytz.UTC)
-    utc_time_str = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+@app.route("/api", methods=["GET"])
+def retrieve():
+    if request.method == "POST":
+        return "<h1>Out of Bound</h1>", 400
+    else:
+        name = request.args.get('slack_name')
+        trck = request.args.get('track')
+       
+        if name is not None and trck is not None:
+            if name == slack_name and trck == track:
+                current_day = datetime.datetime.now().strftime('%A')
+                utc_time = datetime.datetime.now(pytz.UTC)
+                utc_time_str = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    # Status Code of Success (HTTP 200)
-    status_code = 200
+                # Status Code of Success (HTTP 200)
+                status_code = 200
 
-    # JSON response
-    response_data = {
-        "slack_name": slack_name_param,
-        "current_day": current_day,
-        "utc_time": utc_time_str,
-        "track": track_param,
-        "github_file_url": github_file_url,
-        "github_repo_url": github_repo_url,
-        "status_code": status_code
-    }
+                # JSON response
+                response_data = {
+                    "slack_name": slack_name,
+                    "current_day": current_day,
+                    "utc_time": utc_time_str,
+                    "track": track,
+                    "github_file_url": github_file_url,
+                    "github_repo_url": github_repo_url,
+                    "status_code": status_code
+                }
 
-    return jsonify(response_data)
+                return jsonify(response_data)
+            else:
+                return "Fatal error; Wrong Credentials", 401
+        else:
+            return "Error; missing a vital credential", 400
 
 if __name__ == "__main__":
     print("Starting the Flask application...")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
